9ca8630beb17b8b3f7037c4230ffc8b50168c9cb
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2026-01-26 09:41:18 +0100

    feat: habilita o uso do Bootstrap 4 para paginação

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Pagination\Paginator;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFour();
+
         Gate::define('admin', function (User $user) {
             return $user->isAdmin();
         });
